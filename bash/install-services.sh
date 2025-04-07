eb6007b2e105953bdabf9ec420566de8dc2f3392
Ari Yeger <yegera1@newpaltz.edu>
2025-04-07 18:34:26 -0400

    feat: better services

--- bash/install-services.sh
+++ bash/install-services.sh
@@ -1,57 +1,76 @@
 #!/bin/bash
 # This script installs the services and scripts for Prusa Connect Camera and QView3D
-BASE_DIR="${1-$HOME/src}"
-SERVICES_REPO_DIR="$BASE_DIR/services-repo"
-SERVICES_DIR="$SERVICES_REPO_DIR/services"
-SCRIPTS_DIR="$SERVICES_REPO_DIR/scripts"
+SERVICES_REPO_BASE_URL="https://raw.githubusercontent.com/L10nhunter/QViewOS/refs/heads/main/"
+SERVICES_URL="$SERVICES_REPO_BASE_URL/services"
+SCRIPTS_URL="$SERVICES_REPO_BASE_URL/bash"
 SYSTEMD_DIR="/etc/systemd/system"
-SERVICES_REPO_URL="https://github.com/L10nhunter/QViewOS.git"
 BIN_DIR="/usr/local/bin"
-# Clone services repository
-if [[ ! -d "$SERVICES_REPO_DIR" ]]; then
-    echo "Cloning services and scripts repository..."
-    git clone "$SERVICES_REPO_URL" "$SERVICES_REPO_DIR"
-else
-    echo "Services repository already exists, pulling latest changes..."
-    cd "$SERVICES_REPO_DIR" || exit 1
-    git pull
-fi
 
-# Move systemd service files to /etc/systemd/system/
-echo "Moving systemd service files..."
-for FILE in "$SERVICES_DIR"/*; do
-    if [ -f "$FILE" ]; then
-        sudo mv "$SERVICES_DIR/$(basename "$FILE")" "$SYSTEMD_DIR/"
-    fi
-done
+for_all_services() {
+    local SERVICE
+    for SERVICE in prusa-camera-connect qview3d-server qview3d-client; do
+        "$1" "$SERVICE"
+    done
+}
 
-# Move bash scripts to /usr/local/bin/
-echo "Moving bash scripts to /usr/local/bin/..."
-for FILE in "$SCRIPTS_DIR"/*; do
-    if [ -f "$FILE" ]; then
-        sudo mv "$SCRIPTS_DIR/$(basename "$FILE")" "$BIN_DIR/"
-        sudo chmod +754 "$BIN_DIR/$(basename "$FILE")"
+for_all_scripts() {
+    local SCRIPT
+    for SCRIPT in run-qview3d-server run-qview3d-client; do
+        "$1" "$SCRIPT.sh"
+    done
+}
+
+# download services from repo with wget
+download_service() {
+    echo "Downloading $1...";
+    if ! wget "$SERVICES_URL/$1.service" -O "$SYSTEMD_DIR/"; then
+        echo "Failed to download $1";
+        exit 1;
     fi
-done
+}
+
+enable_and_start_service() {
+    local SERVICE="$1"
+    sudo systemctl enable "$SERVICE.service"
+    sudo systemctl start "$SERVICE.service"
+}
+
+add_service_to_dietpi_services() {
+    local SERVICE="$1"
+    # Check if the service is already in dietpi-services
+    if grep -q "$SERVICE" "/boot/dietpi/dietpi-services_include_exclude"; then
+        return
+    fi
+    { echo "+ $SERVICE"; } >>"/boot/dietpi/dietpi-services_include_exclude"
+}
+
+# download bash scripts from repo with wget
+download_script() {
+    if ! sudo wget "$SCRIPTS_URL/$1" -O "$BIN_DIR/"; then
+        echo "Failed to download $1";
+        exit 1;
+    fi
+    if ! sudo chmod 754 "$BIN_DIR/$1"; then
+        echo "Failed to set permissions for $1";
+        exit 1;
+    fi
+}
+
+# download services
+echo "Downloading services..."
+for_all_services download_service
+
+# download bash scripts
+echo "Downloading bash scripts..."
+for_all_scripts download_script
 
 # add services to dietpi-services
 echo "Adding services to dietpi-services..."
-{
-    echo "+ prusa-camera-connect"
-    echo "+ qview3d-server"
-    echo "+ qview3d-client"
-} >>"/boot/dietpi/dietpi-services_include_exclude"
+for_all_services add_service_to_dietpi_services
 
 # Reload systemd to recognize new services
 sudo systemctl daemon-reload
 
 # Enable and start services
 echo "Enabling and starting services..."
-for SERVICE in prusa-camera-connect qview3d-server qview3d-client; do
-    sudo systemctl enable "$SERVICE.service"
-    sudo systemctl start "$SERVICE.service"
-done
-
-# cleanup
-echo "Cleaning up..."
-sudo rm -rf "$SERVICES_REPO_DIR"
+for_all_services enable_and_start_service
